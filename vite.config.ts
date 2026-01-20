c8a114ace76f84ac7f5feb82bb28fa315b6147f4
gnfhjdkuydkyfudul-dev <gnfhjdkuydkyfudul@gmail.com>
2026-01-20 12:54:25 +0200

    Simplify Vite config by removing React plugin
    Removed Vite configuration for React plugin.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,3 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // هذه الإضافة تضمن أن روابط الصور والملفات تعمل بشكل صحيح على GitHub Pages
-  base: './', 
-})
+export default {
+  base: './'
+}
